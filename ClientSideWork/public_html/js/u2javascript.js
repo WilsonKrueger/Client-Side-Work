305041bd813cb180696857fd80cb435573e5cfcf
WilsonKrueger <wkrueger@css.edu>
2022-10-20 12:06:27 -0500

    Setup assignment 2

---
+++ ClientSideWork/public_html/js/u2javascript.js
@@ -0,0 +1,25 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+//$(document).ready(function(){
+//    // Get value on button click and show alert
+//    $("#myBtn").click(function(){
+//        var str = $("#myInput").val();
+//        alert(str);
+//    });
+//});
+$(document).ready(function(){
+//    let items = 
+    
+    $("#addButton").click(function()
+    {
+        let item = $('#itemInput').val();
+        alert(item);
+    
+//        $('#list').append($('<li>', {
+//             text: $('#item').val()
+//        }));
+    });
+});
+
