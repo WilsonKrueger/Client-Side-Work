7bfda9cce068451293595df4e2a4ad9a26ba5191
WilsonKrueger <wkrueger@css.edu>
2022-10-21 09:19:58 -0500

    get jquery working properly

--- ClientSideWork/public_html/js/u2javascript.js
+++ ClientSideWork/public_html/js/u2javascript.js
@@ -2,24 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-//$(document).ready(function(){
-//    // Get value on button click and show alert
-//    $("#myBtn").click(function(){
-//        var str = $("#myInput").val();
-//        alert(str);
-//    });
-//});
+
 $(document).ready(function(){
-//    let items = 
-    
     $("#addButton").click(function()
     {
-        let item = $('#itemInput').val();
-        alert(item);
+//        var item = $('#itemInput').val();
+//        alert(item);
     
-//        $('#list').append($('<li>', {
-//             text: $('#item').val()
-//        }));
+        $('#list').append($('<li>', {
+             text: $('#itemInput').val()
+        }));
+        
+        $("#list li:nth-child(odd)").css('background-color', 'white');
+        $("#list li:nth-child(even)").css('background-color', 'blue');
     });
+    
+
 });
 
