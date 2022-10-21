af6ae45834bddcdd8154c8caedbdf8562fb2aea1
WilsonKrueger <wkrueger@css.edu>
2022-10-21 11:42:53 -0500

    finish assignment 2

--- ClientSideWork/public_html/js/u2javascript.js
+++ ClientSideWork/public_html/js/u2javascript.js
@@ -6,15 +6,21 @@
 $(document).ready(function(){
     $("#addButton").click(function()
     {
-//        var item = $('#itemInput').val();
-//        alert(item);
-    
         $('#list').append($('<li>', {
              text: $('#itemInput').val()
         }));
         
+        //Alternate the colors of the list
         $("#list li:nth-child(odd)").css('background-color', 'white');
-        $("#list li:nth-child(even)").css('background-color', 'blue');
+        $("#list li:nth-child(even)").css('background-color', 'lightblue');
+        
+        //Clear the form
+        $('#mainForm')[0].reset();
+    });
+    
+    $("#removeButton").click(function()
+    {
+        $('#list li:last-child').remove();
     });
     
 
