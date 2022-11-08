4fcf4da602c5def54633dfe08a8489c343cda2d1
WilsonKrueger <wkrueger@css.edu>
2022-11-08 12:39:10 -0600

    start making modal

--- ClientSideWork/public_html/js/u4javascript.js
+++ ClientSideWork/public_html/js/u4javascript.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    $("#accordion").accordion();
+    $('#accordion').accordion({
+        collapsible: true,
+        active: false,
+        width: 'fill',
+        header: 'h3'
+    }).sortable({
+        items: '.panel'
+    });
+    
+//    var modal = $("#myModal").val();
+
+    var modal = document.getElementById("myModal");
+    
+    modal.style.display = "none";    
+    
+//    function setUpModal() {
+//        let fadf
+//    }
  });
