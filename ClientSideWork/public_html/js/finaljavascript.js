940fb9325306c72f4523a25a8aa63862556ded36
WilsonKrueger <wkrueger@css.edu>
2022-12-02 12:21:57 -0600

    setting up quiz

--- ClientSideWork/public_html/js/finaljavascript.js
+++ ClientSideWork/public_html/js/finaljavascript.js
@@ -1,14 +1,32 @@
 $(document).ready(function()
 {    
-    $("#q1Button").click(function()
+//    let nextQuestion = 1;
+    
+    $("#nextButton").click(function()
     {
-        $( "#questionMainDiv" ).load( "question1.html #question1Div" );
+        let nextQ = $( "#nextQ" ).val();
+        let questionHTML = "question" + nextQ + ".html";
+        $( "#questionMainDiv" ).load( questionHTML );
+        
+//        nextQuestion++;
     });
+    
+//    $("#q1Button").click(function()
+//    {
+//        $( "#questionMainDiv" ).load( "question1.html" );
+////        window.location = "/question1.html";
+//    });
+//    
+//    $("#q2Button").click(function()
+//    {
+//        $( "#questionMainDiv" ).load( "question2.html" );
+//    });
      
+    $( "#datepicker" ).datepicker();
+
     $( function() {
-    $( "#datepicker" ).datepicker({
-      changeMonth: true,
-      changeYear: true
+    $( "#progressbar" ).progressbar({
+      value: 50
     });
   } );
 });
