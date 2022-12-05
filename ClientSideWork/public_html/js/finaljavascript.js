90b1f5adc402cfba390423c14d4b931c0477a015
WilsonKrueger <wkrueger@css.edu>
2022-12-04 22:57:38 -0600

    make progressbar work

--- ClientSideWork/public_html/js/finaljavascript.js
+++ ClientSideWork/public_html/js/finaljavascript.js
@@ -1,57 +1,100 @@
 $(document).ready(function()
 {    
-//    let nextQuestion = 1;
+    var progressbarDiv = $( "#progressbar" ).progressbar();
+    var progressbarLabel = $( ".progress-label" );
+    $( "#datepicker" ).datepicker();
     
-    $("#nextButton").click(function()
+    var numberCorrect = 1;
+    var userAnswer;
+    var correctAnswer;
+    var q1Correct = true;
+    var q2Correct = false;
+    var q3Correct = false;
+    var q4Correct = false;
+    var q5Correct = false;
+
+    $("#navButton").click(function()
     {
-        let nextQ = $( "#nextQ" ).val();
-        let questionHTML = "question" + nextQ + ".html";
-        $( "#questionMainDiv" ).load( questionHTML );
+        var navIndex = parseInt($( "#navIndex" ).val());
+        let questionHTML;
         
-        let progress = 20 * (nextQ - 1);
+        //Validate questions 1-5
+        if(navIndex === 2)
+        {
+            validateQ1();
+        }
+//        else if(navIndex === 3)
+//        {
+//            validateQ2();
+//        }
+//        else if(navIndex === 4)
+//        {
+//            validateQ3();
+//        }
+//        else if(navIndex === 5)
+//        {
+//            validateQ4();
+//        }
         
-//        $( "#progressbar" ).progressbar({ value: progress});
-//        $( "#progressbar" ).progressbar( "value", progress );
-        $( "#progressbar" ).progressbar( "option", "value", progress );
-        $( ".progress-label" ).text(progress + "%");
+        if(navIndex === 6)
+        {
+//            validateQ5();
+            calculateResults();
+        }
+        else if(navIndex === 0)
+        {
+            questionHTML = "finalMain.html #questionMainDiv";
+        }
+        else 
+        {
+            questionHTML = "question" + navIndex + ".html #questionDiv";
+        }
         
-//        nextQuestion++;
+        // Load the proper html
+        $("#questionMainDiv").load(questionHTML ,function(){
+            $.getScript("../js/finaljavascript.js"); 
+        });
+        
+        // Value to update the progress bar
+        var progress = 20 * (navIndex - 1);
+        
+        // Reset progress to 0 if going to main page
+        if(navIndex === 0)
+        {
+            progress = 0;
+        }
+
+        // Update the progress bar and label 
+        progressbarDiv.progressbar({
+            value: parseInt(progress)
+        });
+        progressbarLabel.text(progress + "%");
+        
+        
     });
     
-//    $( function() {
-//    var progressbar = $( "#progressbar" ),
-//      progressLabel = $( ".progress-label" );
-// 
-//    progressbar.progressbar({
-//      value: false,
-//      change: function() {
-//        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
-//      },
-//      complete: function() {
-//        progressLabel.text( "Complete!" );
-//      }
-//    });
+    function validateQ1() 
+    {
+        userAnswer = $("#datepicker").val();
+        correctAnswer = "08/26/2022";
+        
+        if(userAnswer === correctAnswer)
+        {
+            numberCorrect++;
+            q1Correct = true;
+        }
+    }
     
-//    $("#q1Button").click(function()
-//    {
-//        $( "#questionMainDiv" ).load( "question1.html" );
-////        window.location = "/question1.html";
-//    });
-//    
-//    $("#q2Button").click(function()
-//    {
-//        $( "#questionMainDiv" ).load( "question2.html" );
-//    });
-     
-    $( "#datepicker" ).datepicker();
-
-    $( "#progressbar" ).progressbar();
+    function calculateResults() 
+    {
+        $("#score").text("Score: " + numberCorrect + "/5");
+        if(q1Correct)
+        {
+            $("#q1Results").text("You got question 1 correct!");
+        }
+    }
     
-//    $( function() {
-//        $( "#progressbar" ).progressbar({
-//          value: 50
-//        });
-//    } );
+    //Method to load the results
 });
 
 
